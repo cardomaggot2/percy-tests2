c05ca3ca638d85c7be28f92a52f527ca84dbd01e
cardomaggot2 <cardomaggot@gmail.com>
2025-06-30 23:35:57 -0600

    adding more timeout and retries

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+  retries: 1,
+  timeout: 60000,
   use: {
     headless: true,
     viewport: { width: 1280, height: 720 },
