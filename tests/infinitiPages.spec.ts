828e7e95214749d757e28acd43aab8e40fcf4359
cardomaggot2 <cardomaggot@gmail.com>
2025-06-30 22:54:18 -0600

    Duplicate percy error fix

--- tests/infinitiPages.spec.ts
+++ tests/infinitiPages.spec.ts
@@ -8,17 +8,19 @@ type RouteGroups = {
 };
 
 const BASE_URL = 'https://www.infinitiusa.com';
-
-
 const routeGroups: RouteGroups = routes;
- 
+
 for (const [section, paths] of Object.entries(routeGroups)) {
   test.describe(`${section} pages`, () => {
     for (const route of paths) {
       test(`Snapshot of ${route}`, async ({ page }) => {
         await page.goto(`${BASE_URL}${route}`);
         //await page.waitForLoadState('networkidle');
-        await percySnapshot(page, `${section} - ${route}`);
+        // ✅ Clean the route to use as a unique snapshot name
+        const cleanRoute = route.replace(/\//g, '-').replace(/^-/, '');
+        if(cleanRoute.includes('/shopping-tools/build-price')) {
+          await percySnapshot(page, `${section} - ${cleanRoute}`);
+        }
       });
     }
   });
